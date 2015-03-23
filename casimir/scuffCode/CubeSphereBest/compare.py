8dd5af1f24980698a3aab385676bb325b1a4c9d2
Noah Kurinsky <nkurinsky91@gmail.com>
2015-03-23 10:50:04 -0700

    more results, figure

--- casimir/scuffCode/CubeSphereBest/compare.py
+++ casimir/scuffCode/CubeSphereBest/compare.py
@@ -14,6 +14,18 @@ d1t=d1t[inds]
 f1t=f1t[inds]
 g1t=g1t[inds]
 
+d2t,g2t,e2t,ee2t,f2t,ef2t,s2t=numpy.loadtxt("combined_results_temp.txt",unpack=True,skiprows=1)
+f2t=-f2t*31.6e-15
+inds=argsort(d2t)
+d2t=d2t[inds]
+f2t=f2t[inds]
+g2t=g2t[inds]
+s2t=s2t[inds]
+inds=numpy.where(s2t == 0)
+d2t=d2t[inds]
+f2t=f2t[inds]
+g2t=g2t[inds]
+
 d3,e3,ee3,f3,ef3=numpy.loadtxt("../Comparison/full.txt",unpack=True)
 f3=-f3*31.6e-15
 inds=argsort(d3)
@@ -38,6 +50,10 @@ inds = numpy.where(g1t == gst)
 plot(d1t[inds],f1t[inds],'-.',label="PEC 300K, grid="+str(gst),color="black")
 inds = numpy.where(g1t == 0.4)
 #plot(d1t[inds],f1t[inds],'-.',label="PEC 300K, grid="+str(0.4),color="orange")
+
+gs=numpy.min(g2t)
+inds = numpy.where(g2t == gs)
+plot(d2t[inds],f2t[inds],'-.',label="FEC 300K, grid="+str(gs),color="green")
 
 plot(d4,f4,':',label="PEC, Large Cantilever",color="black")
 plot(d3,f3,':',label="FEC, Large Cantilever",color="green")
