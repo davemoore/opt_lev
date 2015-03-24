09b14ce3af6862151149d30d8f425fe420b0f168
Noah Kurinsky <nkurinsky91@gmail.com>
2015-03-24 12:34:36 -0400

    adding plots and plotting script

---
+++ casimir/scuffCode/plot_all.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+pushd CubeSphere
+python compare.py
+popd
+
+pushd CubeSphereAspect
+python width.py
+python width_finite.py
+popd
+
+pushd CubeSphereBest
+python compare.py
+popd
+
+pushd CubeSphereDepth
+python width.py
+python width_finite.py
+popd
+
+pushd CubeSphereLateral
+python trend.py
+python trend_finite.py
+popd
