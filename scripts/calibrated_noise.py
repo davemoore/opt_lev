78d099acc190823385aa87fd971e4f3c511b30ed
charlesblakemore <chas.blakemore@gmail.com>
2015-09-10 11:17:05 -0700

    updated datareadfuns to use new getdata from bead_util. added a bit of modular functionality

--- scripts/calibrated_noise.py
+++ scripts/calibrated_noise.py
@@ -7,12 +7,13 @@ import numpy as np
 import bead_util as bu
 
 #path = r"C:\Data\20150825\Bead1"
-path = "/data/20150903/Bead1/chargecal/urmbar_xyzcool.h5"
+path = "/data/20150909/Bead1/redischarge/" \
+       + "URmbar_xyzcool_nofilters3_elec3_250mV41Hz250mVdc_20.h5"
 
-NFFT = 2**17
+NFFT = 2**18
 
 drive_freq = 41
-drive_voltage = 80
+drive_voltage = 50
 
 trap_dim = 4e-3
 
@@ -21,21 +22,24 @@ e0 = 1.602e-19
 
 	
 data, attribs, _  = bu.getdata(path)
-Fs = 5000 #attribs['Fsamp']
+Fs = attribs['Fsamp']
 
-psd, freqs = mlab.psd(data[:,1], Fs=Fs, NFFT=NFFT)
+ypsd, freqs = mlab.psd(data[:,1], Fs=Fs, NFFT=NFFT)
+print freqs
+
+binwidth = freqs[1]-freqs[0]
 
 index = np.argmin(np.abs(freqs - drive_freq))
 
-drive_power = psd[index]
-
-f = open('response_at_drive.txt', 'w')
-f.write(np.sqrt(drive_power))
-f.close()
-
+response_power = ypsd[index] * binwidth
      
 force = (drive_voltage / trap_dim) * (e0 * e_charges)
 
-conv = force / np.sqrt(drive_power)
+conv = force / np.sqrt(response_power)
 
+print conv, "(Newton / Volt)"
+
+f = open('volt_to_newton_conv.txt', 'w')
+f.write(str(conv))
+f.close()
 
--- scripts/datareadfunsxyz.py
+++ scripts/datareadfunsxyz.py
@@ -1,14 +1,17 @@
 import numpy, h5py
 import matplotlib
 import matplotlib.pyplot as plt
+import matplotlib.mlab as mlab
 import os
 import scipy.signal as sp
 import numpy as np
+import bead_util as bu
 
-refname = r"1mbar_xyzcool.h5"
-fname0 = r""
-path = r"C:\Data\20150825\Bead1"
-d2plt = 1
+refname = "URmbar_xyzcool_nofilters3_elec3_250mV41Hz250mVdc_20.h5"
+fname0 = "URmbar_xyzcool_nofilters3_elec3_250mV41Hz250mVdc_55.h5"
+#path = r"C:\Data\20150825\Bead1"
+path = "/data/20150909/Bead1/recharge_cal"
+d2plt = 0
 if fname0 == "":
 	filelist = os.listdir(path)
 
@@ -22,77 +25,54 @@ if fname0 == "":
  
 	fname0 = mrf		
 
-
 		 
 
-Fs = 5e3  ## this is ignored with HDF5 files
 NFFT = 2**12
 
-def getdata(fname):
-	print "Opening file: ", fname
-	## guess at file type from extension
-	_, fext = os.path.splitext( fname )
-	if( fext == ".h5"):
-		f = h5py.File(fname,'r')
-		dset = f['beads/data/pos_data']
-		dat = numpy.transpose(dset)
-		#max_volt = dset.attrs['max_volt']
-		#nbit = dset.attrs['nbit']
-		Fs = dset.attrs['Fsamp']
-		
-		#dat = 1.0*dat*max_volt/nbit
+def getpsd(data, attribs):
+        Fs = attribs['Fsamp']
+        xpsd, freqs = mlab.psd(data0[:,0], Fs=Fs, NFFT=NFFT)
+        ypsd, freqs = mlab.psd(data0[:,1], Fs=Fs, NFFT=NFFT)
+        zpsd, freqs = mlab.psd(data0[:,2], Fs=Fs, NFFT=NFFT)
+        return [freqs, xpsd, ypsd, zpsd]
 
-	else:
-		dat = numpy.loadtxt(fname, skiprows = 5, usecols = [2, 3, 4, 5, 6] )
-
-	xpsd, freqs = matplotlib.mlab.psd(dat[:, 0]-numpy.mean(dat[:, 0]), Fs = Fs, NFFT = NFFT) 
-	ypsd, freqs = matplotlib.mlab.psd(dat[:, 1]-numpy.mean(dat[:, 1]), Fs = Fs, NFFT = NFFT)
-        zpsd, freqs = matplotlib.mlab.psd(dat[:, 2]-numpy.mean(dat[:, 2]), Fs = Fs, NFFT = NFFT)
-
-	norm = numpy.median(dat[:, 2])
-        #for h in [xpsd, ypsd, zpsd]:
-        #        h /= numpy.median(dat[:,2])**2
-	return [freqs, xpsd, ypsd, dat, zpsd]
-
-data0 = getdata(os.path.join(path, fname0))
-
-def rotate(vec1, vec2, theta):
-    vecn1 = numpy.cos(theta)*vec1 + numpy.sin(theta)*vec2
-    vecn2 = numpy.sin(theta)*vec1 + numpy.cos(theta)*vec2
-    return [vec1, vec2]
-
+data0, attribs0, handle0 = bu.getdata(os.path.join(path, fname0))
+freqs0, xpsd0, ypsd0, zpsd0 = getpsd(data0, attribs0)
 
 if refname:
-	data1 = getdata(os.path.join(path, refname))
-Fs = 10000
-b, a = sp.butter(1, [2*5./Fs, 2*10./Fs], btype = 'bandpass')
+	data1, attribs1, handle1 = bu.getdata(os.path.join(path, refname))
+        freqs1, xpsd1, ypsd1, zpsd1 = getpsd(data1, attribs1)
 
 if d2plt:	
 
         fig = plt.figure()
-        plt.plot(data0[3][:, 0])
-        plt.plot(data0[3][:, 1])
-        plt.plot(data0[3][:, 3])
+        plt.plot(data0[:, 0])
+        plt.plot(data0[:, 1])
+        plt.plot(data0[:, 3])
        # plt.plot(np.abs(data0[3][:, 3])-np.mean(np.abs(data0[3][:, 3])))
        
+f = open('volt_to_newton_conv.txt')
+conv = float(f.readline())
 
-
+print conv
 
 fig = plt.figure()
 plt.subplot(3, 1, 1)
-plt.loglog(data0[0], data0[1],label="Data")
+plt.loglog(freqs0, np.sqrt(xpsd0)*conv,label="Data")
 if refname:
-	plt.loglog(data1[0], data1[1],label="Ref")
-plt.ylabel("V$^2$/Hz")
+	plt.loglog(freqs1, np.sqrt(xpsd1)*conv,label="Ref")
+plt.ylabel("N/rt(Hz)")
+#plt.ylabel("V$^2$/Hz")
 plt.legend()
 plt.subplot(3, 1, 2)
-plt.loglog(data0[0], data0[2])
+plt.loglog(freqs0, np.sqrt(ypsd0)*conv)
 if refname:
-	plt.loglog(data1[0], data1[2])
+	plt.loglog(freqs1, np.sqrt(ypsd1)*conv)
 plt.subplot(3, 1, 3)
-plt.loglog(data0[0], data0[4])
+plt.loglog(freqs0, np.sqrt(zpsd0) * conv)
 if refname:
-	plt.loglog(data1[0], data1[4])
-plt.ylabel("V$^2$/Hz")
+	plt.loglog(freqs1, np.sqrt(zpsd1) * conv)
+plt.ylabel("N/rt(Hz)")
+#plt.ylabel("V$^2$/Hz")
 plt.xlabel("Frequency[Hz]")
 plt.show()
