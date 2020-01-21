5c0b21f3e40abc2bdaf743052c5b352d0d8f095d
David Moore <david.c.moore@yale.edu>
2020-01-21 08:15:07 -0500

    Updates to fit model

--- scripts/temp_check/photopheresis/plot_force.py
+++ scripts/temp_check/photopheresis/plot_force.py
@@ -7,12 +7,12 @@ r = 7.5e-6 # radius, m
 Rgas = 8.3 # J/(mol K)
 eta = 1.8e-5 # Pa s
 Mgas = 0.028 # kg/mol
-dT = 0.5 #K
+dT = 0.005 #K
 Tgas = 300 #K
 gam = 1
 kb = 1.38e-23 # J/K
-msph = 1e-12 # kg
-omega0 = 2*np.pi*50 # rad/s
+msph = 3.2e-12 # kg
+omega0 = 2*np.pi*150 # rad/s
 
 def mean_free_path(vis, press, temp, Mgas):
     L1 = vis/press
@@ -46,6 +46,9 @@ def Kf( p, gam ):
     #print( "fs p0: ", 3*eta*np.sqrt(Rgas*Tgas/(Mgas*gam))/r )
     return np.sqrt( np.abs(A*B) )/(p/p0 + p0/p)
 
+print("force: ", Kf( 1e5, 0.6))
+
+
 # plt.figure()
 # plt.loglog( pp/1e2, Gam0(pp) )
 # plt.show()
@@ -59,7 +62,7 @@ def P_at_fall(p, gam, E_0, alpha, beta, gamma):
     #P = (np.sqrt(np.abs(beta**2 + 4*E_0*(G+gamma)**2*msph*omega0**2 - 4*alpha*(G+gamma)*msph*omega0**2)) - beta)/(2*Fth*(G+gamma))
     P = (msph*omega0**2/(2*Fth**2)) * (np.sqrt( (Fth**2*gamma**2)/((alpha+G+beta+gamma)**2*msph**2*omega0**4) + 4*Fth**2/(msph*omega0**2)*(E_0 - G*kb*Tgas/(alpha+G+beta+gamma) - beta/(alpha+G+beta+gamma))) - Fth*gamma/((alpha + G+beta+gamma)*msph*omega0**2) )
     
-    P[  beta/(alpha+G) >= E_0 ] = 0
+    #P[  beta/(alpha+G) >= E_0 ] = 0
 
     for0 = G/(G+alpha+beta+gamma)*kb*Tgas
     for1 = beta/(G+alpha+beta+gamma) 
@@ -126,7 +129,7 @@ x = x*1e2 # it is in pascal
 
 const = 1e-13
 
-ffn_simp = lambda xx, C, ac, E_0: C*P_at_fall(xx, ac, E_0, 0, 0, 0)
+ffn_simp = lambda xx, ac, E_0: P_at_fall(xx, ac, E_0, 0, 0, 0)
 ffn_simp1 = lambda xx, C, ac, E_0, beta: C*P_at_fall(xx, ac, E_0, 0, beta, 0)
 ffn = lambda xx, C, ac, E_0, alpha, beta, gamma: C*P_at_fall(xx, ac, E_0, 0, beta, gamma)
 
@@ -136,9 +139,14 @@ spars = [ 4.73735391e+00,  3.87456239e-01,  5.56661460e-15, -5.26259165e+00,
 #errs[errs == 0] = 1
 
 gpts = x > 200
-bp_simp, bcov_simp = opt.curve_fit( ffn_simp, x[gpts], y[gpts], p0=spars[0:3])
+bp_simp, bcov_simp = opt.curve_fit( ffn_simp, x[gpts], y[gpts], p0=spars[1:3])
 #bp_simp = spars[0:3]
 
+print( "C,ac,E:", bp_simp, bp_simp[-1]/kb )
+
+perr = np.sqrt(np.diag(bcov_simp))
+print(perr)
+print("Trap depth [K]: %.1e %.1e"%(bp_simp[-1]/kb, np.sqrt(bcov_simp[1,1])/kb))
 gpts = x > 0
 bp, bcov = opt.curve_fit( ffn, x[gpts], y[gpts], p0=spars )
 #bp = spars
